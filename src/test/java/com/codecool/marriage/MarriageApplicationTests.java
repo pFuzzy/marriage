9b3fec9773e8fd1cf85be102db04fa38610e439e
pFuzzy <p.fazii888@gmail.com>
2021-03-25 11:23:59 +0100

    initial commit

---
+++ src/test/java/com/codecool/marriage/MarriageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.marriage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarriageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
